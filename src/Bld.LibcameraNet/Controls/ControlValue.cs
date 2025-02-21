1d619c30ff4165f4853328c1ca35c7a1c07363ff
Roman Buldygin <froooks@gmail.com>
2025-02-21 20:49:36 +0300

    More controls parsers

--- src/Bld.LibcameraNet/Controls/ControlValue.cs
+++ src/Bld.LibcameraNet/Controls/ControlValue.cs
@@ -38,3 +38,59 @@ public class StringControlValue<TIdEnum>
         Value = Marshal.PtrToStringAnsi(dataPtr, definition.NumElements);
     }
 }
+
+public class BoolControlValue<TIdEnum>
+    : ControlValue<TIdEnum, bool> where TIdEnum : struct, Enum
+{
+    public BoolControlValue(ControlValueDefinition<TIdEnum> definition)
+        : base(definition)
+    {
+        var dataPtr = LibcameraNative.ControlValueGet(definition.ValuePtr);
+        Value = Marshal.ReadByte(dataPtr) != 0;
+    }
+}
+
+public class ByteControlValue<TIdEnum>
+    : ControlValue<TIdEnum, byte> where TIdEnum : struct, Enum
+{
+    public ByteControlValue(ControlValueDefinition<TIdEnum> definition)
+        : base(definition)
+    {
+        var dataPtr = LibcameraNative.ControlValueGet(definition.ValuePtr);
+        Value = Marshal.ReadByte(dataPtr);
+    }
+}
+
+public class Integer32ControlValue<TIdEnum>
+    : ControlValue<TIdEnum, Int32> where TIdEnum : struct, Enum
+{
+    public Integer32ControlValue(ControlValueDefinition<TIdEnum> definition)
+        : base(definition)
+    {
+        var dataPtr = LibcameraNative.ControlValueGet(definition.ValuePtr);
+        Value = Marshal.ReadInt32(dataPtr);
+    }
+}
+
+public class Integer64ControlValue<TIdEnum>
+    : ControlValue<TIdEnum, Int64> where TIdEnum : struct, Enum
+{
+    public Integer64ControlValue(ControlValueDefinition<TIdEnum> definition)
+        : base(definition)
+    {
+        var dataPtr = LibcameraNative.ControlValueGet(definition.ValuePtr);
+        Value = Marshal.ReadInt32(dataPtr);
+    }
+}
+
+public class FloatControlValue<TIdEnum>
+    : ControlValue<TIdEnum, float> where TIdEnum : struct, Enum
+{
+    public FloatControlValue(ControlValueDefinition<TIdEnum> definition)
+        : base(definition)
+    {
+        var dataPtr = LibcameraNative.ControlValueGet(definition.ValuePtr);
+        // TODO
+        Value = 0;
+    }
+}
--- src/Bld.LibcameraNet/Controls/ControlValueFactory.cs
+++ src/Bld.LibcameraNet/Controls/ControlValueFactory.cs
@@ -9,11 +9,11 @@ internal static class ControlValueFactory<TId> where TId: struct, Enum
         return definition.Type switch
         {
             ControlType.ControlTypeNone => new ControlValue<TId>(definition),
-            ControlType.ControlTypeBool => new ControlValue<TId>(definition),
-            ControlType.ControlTypeByte => new ControlValue<TId>(definition),
-            ControlType.ControlTypeInteger32 => new ControlValue<TId>(definition),
-            ControlType.ControlTypeInteger64 => new ControlValue<TId>(definition),
-            ControlType.ControlTypeFloat => new ControlValue<TId>(definition),
+            ControlType.ControlTypeBool => new BoolControlValue<TId>(definition),
+            ControlType.ControlTypeByte => new ByteControlValue<TId>(definition),
+            ControlType.ControlTypeInteger32 => new Integer32ControlValue<TId>(definition),
+            ControlType.ControlTypeInteger64 => new Integer64ControlValue<TId>(definition),
+            ControlType.ControlTypeFloat => new FloatControlValue<TId>(definition),
             ControlType.ControlTypeString => new StringControlValue<TId>(definition),
             ControlType.ControlTypeRectangle => new ControlValue<TId>(definition),
             ControlType.ControlTypeSize => new ControlValue<TId>(definition),
